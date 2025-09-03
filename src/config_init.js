dafafa7fcd6ca14cb87e43541e2c1ba71b0d1f1f
User Opensource <bawdyanarchist@protonmail.com>
2025-09-03 16:36:23 -0600

    Add sample file warnings, remove those comments in working env files

--- src/config_init.js
+++ src/config_init.js
@@ -25,7 +25,13 @@ const configFiles = [
 for (const file of configFiles) {
    if (!fs.existsSync(file.live)) {
       if (fs.existsSync(file.bak)) {
-         fs.copyFileSync(file.bak, file.live);
+         fs.copyFileSync(file.bak, file.live);   // Copy the file
+         /* Remove the warning comments at the top */
+         const lines = fs.readFileSync(file.live, 'utf8').split('\n');
+         if (lines[0].startsWith('##') && lines[1].startsWith('##')) {
+            const modifiedContent = lines.slice(2).join('\n');
+            fs.writeFileSync(file.live, modifiedContent, 'utf8');
+         }
       } else {
          throw new Error(`[INIT] FATAL: ${file.live} not found and no example exists at ${file.bak}`);
       }
