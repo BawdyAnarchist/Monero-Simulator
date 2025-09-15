87d13c92f9996874bdb1048f4321748d2e745c51
User Opensource <bawdyanarchist@protonmail.com>
2025-09-15 11:20:39 -0600

    Selfish modules being rolled into 1 configurable/generalizable module

--- src/plugins/selfish_sniper.js
+++
@@ -1,9 +0,0 @@
-/*
-   The Selfish Sniper publishes some of their blocks to maintain parity with the honest chain,
-   siphoning off honest hashpower to mine on stale blocks. If they ever fall behind, they publish
-   new blocks in their reserve, until the moment their lead is about to correct back to normal.
-*/
-
-export function invokeStrategySS(activeEvent) {
-return;
-}
--- src/plugins/silent_reorg.js
+++
@@ -1,7 +0,0 @@
-/*
-   The Silent Reorg withholds all of their blocks until the moment they reorg the chain.
-*/
-
-export function invokeStrategySR(activeEvent) {
-;
-}
